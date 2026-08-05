b94d55bd59f71f8359c241128a91150978721642
tbtbm2567 <tbtbm2567@gmail.com>
2026-08-05 12:58:21 +0700

    Create api-config.js

---
+++ api-config.js
@@ -0,0 +1,5 @@
+// ============================================================
+//  ตั้งค่า Google Apps Script Web App URL
+//  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
+// ============================================================
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbxMb8U3ar1XDtXYQeRvXGHl5EZWrv3eu6BeZjHFi6BkSJNknSVQ9aL8IweumnfvYS8ovw/exec';
